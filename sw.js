9884eed88979c3efa5c24b03c1346e6f63afad58
gurpr337 <gurpr337@gmail.com>
2021-10-29 15:53:13 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3f4bd0c76b71ec4da927.js"
+    "url": "webpack-runtime-e324219b946b035011f9.js"
   },
   {
     "url": "framework-39511ec02ffa59838704.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "efe17c2014a01c930d9850db6195c756"
+    "revision": "c2d336983e12917c1c7a7012aab5507b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ac40d2a65f785bd0874e.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f0b36a61f78c18b776e274a81d333549"
+    "revision": "3a96c71b0ef58397d256f32d21b5f09b"
   },
   {
     "url": "manifest.webmanifest",
